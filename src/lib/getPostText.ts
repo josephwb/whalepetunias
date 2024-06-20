afa1c88866ef75cb6497f7e74723ee67a3f765ba
Joseph Brown <phylo.jwb@gmail.com>
2024-06-20 14:36:21 -0400

    i have no idea what i am doing here...

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -1,4 +1,22 @@
+import * as fs from 'fs';
+
 export default async function getPostText() {
   // Generate the text for your post here. You can return a string or a promise that resolves to a string
-  return "Hello from the Bluesky API";
+  
+  // read entire file into an array, pick random line to return
+  fs.readFileSync('./example.txt','utf8');
+  var array = fs.readFileSync('./example.txt','utf8').toString().split("\n");
+
+  // choose one of the lines...
+  //var line = array[Math.floor(Math.random()*array.length)]
+  var x = Math.floor(Math.random() * array.length);
+  var line = array[x];
+
+  //console.log(array.length);
+  console.log(x);
+  console.log(array);
+
+  return line;
+
+  //return "Hello from the Bluesky API";
 }
