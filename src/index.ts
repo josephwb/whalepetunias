c1e54d69af469eb1f2c7a633d09a7c7e91b21136
Joseph Brown <phylo.jwb@gmail.com>
2024-06-22 20:01:36 -0400

    prelim text, filtering, etc.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import Bot from "./lib/bot.js";
 import getPostText from "./lib/getPostText.js";
 
-//const text = await Bot.run(getPostText, { dryRun: true });
-const text = await Bot.run(getPostText);
+const text = await Bot.run(getPostText, { dryRun: true });
+//const text = await Bot.run(getPostText);
 
 console.log(`[${new Date().toISOString()}] Posted: "${text}"`);
--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -4,23 +4,23 @@ export default async function getPostText() {
   // Generate the text for your post here. You can return a string or a promise that resolves to a string
   
   // read entire file into an array, pick random line to return
-  fs.readFileSync('./example.txt','utf8');
-  var array = fs.readFileSync('./example.txt','utf8').toString().split("\n");
+ // fs.readFileSync('./text.txt','utf8');
+  var array = fs.readFileSync('./text.txt','utf8').toString().split("\n");
   
   //console.log(array.length);
 
   // get rid of empty strings
   array = array.filter(function(n){ return n.length > 0});
-  // get rid of headers e.g. Chapter. use '#' to identify these
+  // get rid of headers e.g. title, chapter, preface, etc. use '#' to identify these
   array = array.filter(function(n){ return !n.startsWith("#")});
   
   // choose one of the lines...
   var x = Math.floor(Math.random() * array.length);
   var line = array[x];
   
-  //console.log(array.length);
+  console.log(array.length);
   console.log(x);
-  console.log(array);
+  //console.log(array);
 
   // implement a (while) check to ensure strings are >= 300 characters?
   // filter command (empty) would work for this
